63b6a85018a7722cb00130cfc9fe63951fb4aa5e
lince666 <valentin.michilena@gmail.com>
2021-10-03 16:24:34 -0300

    usuarioName de session a local

--- js/init.js
+++ js/init.js
@@ -7,7 +7,7 @@ const PRODUCT_INFO_COMMENTS_URL = "https://japdevdep.github.io/ecommerce-api/pro
 const CART_INFO_URL = "https://japdevdep.github.io/ecommerce-api/cart/987.json";
 const CART_BUY_URL = "https://japdevdep.github.io/ecommerce-api/cart/buy.json";
 
-var usuario = sessionStorage.getItem('usuarioName');
+var usuario = localStorage.getItem('usuarioName');
 
 var showSpinner = function(){
   document.getElementById("spinner-wrapper").style.display = "block";
@@ -59,7 +59,7 @@ function goToLogin() {
 
 // Función que asigna null a la variable usuario
 function logOut(){
-  sessionStorage.setItem ('usuarioName', null);
+  localStorage.setItem ('usuarioName', null);
   window.location.replace("login.html");
 }
 
--- js/login.js
+++ js/login.js
@@ -7,5 +7,5 @@ document.addEventListener("DOMContentLoaded", function(e){
 
 function nombreUsuario(){
 var usuario = document.getElementById('username').value;
-sessionStorage.setItem ('usuarioName', usuario);
+localStorage.setItem ('usuarioName', usuario);
 };
